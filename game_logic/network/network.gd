0b48916d97efbf0be179a47df3c694bc1d2a8bb5
Oliver <oliveraoakman@gmail.com>
2024-01-14 15:07:38 +1100

    Movement complete. Migrating minigame behaviour

--- game_logic/network/network.gd
+++ game_logic/network/network.gd
@@ -3,23 +3,52 @@ extends Node2D
 
 const HEX_RADIUS = 300
 const PUZZLE_NODE = preload("res://game_logic/network/puzzle_node.tscn")
+
+var active_node = 0
+
 var node_lookup: Dictionary = {}
+var all_connected_nodes: Dictionary = {}
+var all_backup_nodes: Dictionary = {}
+
+var polyline_spec: PackedVector2Array
 
 func _ready() -> void:
 	_populate_network()
-	node_lookup[3].activate_node()
+	node_lookup[active_node].activate_node()
+	
+	for i in range(6):
+		all_connected_nodes[i] = [wrap(i - 1, 0, 6), wrap(i + 1, 0, 6)]
+		all_backup_nodes[i] = [wrap(i - 2, 0, 6), wrap(i + 2, 0, 6)]
+
+
+func _process(delta: float) -> void:
+	queue_redraw()
+
+
+func _draw() -> void:
+	polyline_spec = _get_lines()
+	draw_polyline(polyline_spec, Color.WEB_GRAY, 5.0, true)
+
+
+func _get_lines() -> PackedVector2Array:
+	var connection_points: PackedVector2Array = []
+	for base_node in all_connected_nodes:
+		if node_lookup[base_node].node_enabled:
+			connection_points.append(node_lookup[base_node].global_position)
+	
+	connection_points.append(connection_points[0])
+	return connection_points
 
 
 func _populate_network() -> void:
 	var base_screen_transform = DisplayServer.window_get_size()
-	print(base_screen_transform)
 	var screen_center = Vector2i(base_screen_transform.x / 2, base_screen_transform.y / 2)
 	
 	var curr_angle: float = 0.0
 	var hex_points: Array = []
 	
 	for _i in range(6):
-		var new_point = Vector2i(HEX_RADIUS * cos(curr_angle), HEX_RADIUS * sin(curr_angle)) + screen_center
+		var new_point = Vector2i(int(HEX_RADIUS * cos(curr_angle)), int(HEX_RADIUS * sin(curr_angle))) + screen_center
 		hex_points.append(new_point)
 		curr_angle += 60 * PI/180
 	
@@ -27,7 +56,43 @@ func _populate_network() -> void:
 	for point in hex_points:
 		var new_node = PUZZLE_NODE.instantiate()
 		add_child(new_node)
-		new_node.position = point
+		new_node.global_position = point
+		new_node.node_base_coord = point
 		node_lookup[curr_idx] = new_node
 		curr_idx += 1
+
+
+func _move_to_node(new_node: int, backup_node: int) -> void:
+	var move_to = new_node
+	if not node_lookup[new_node].node_enabled:
+		move_to = backup_node
+	
+	node_lookup[active_node].deactivate_node()
+	node_lookup[move_to].activate_node()
+	active_node = move_to
+
+
+func _try_move(event: InputEvent) -> void:
+	var connected_nodes = all_connected_nodes[active_node]
+	var backup_nodes = all_backup_nodes[active_node]
+	
+	var node_option = 0
+	for each_node in connected_nodes:
+		var vert_diff = node_lookup[each_node].node_base_coord.y - node_lookup[active_node].node_base_coord.y
+		var horiz_diff = node_lookup[each_node].node_base_coord.x - node_lookup[active_node].node_base_coord.x
+		var backup_node = backup_nodes[node_option]
+		if event.is_action_pressed("move_left") and horiz_diff < 0:
+			_move_to_node(each_node, backup_node)
+		elif event.is_action_pressed("move_right") and horiz_diff > 0:
+			_move_to_node(each_node, backup_node)
 		
+		elif event.is_action_pressed("move_up") and vert_diff < 0:
+			_move_to_node(each_node, backup_node)
+		elif event.is_action_pressed("move_down") and vert_diff > 0:
+			_move_to_node(each_node, backup_node)
+		
+		node_option += 1
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	_try_move(event)
--- game_logic/puzzle_node.gd
+++ game_logic/puzzle_node.gd
@@ -5,13 +5,68 @@ extends Node2D
 const NORMAL_TEXTURE = preload("res://game_logic/network/puzzle_node.svg")
 const ACTIVE_TEXTURE = preload("res://game_logic/network/puzzle_node_active.svg")
 
+var node_base_coord: Vector2i
+var node_enabled: bool = true
+
+var target_position: Vector2
+var close_enough: bool = false
+
+var max_jiggle_size: float = 10.0
+var min_jiggle_time: float = 1.0
+
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+
+
+func _process(delta: float) -> void:
+	_jiggle()
+
+
+func _jiggle() -> void:
+	if node_base_coord == null:
+		return
+	if target_position == Vector2.ZERO or target_position == null:
+		target_position = global_position
+
+	close_enough = global_position.distance_to(target_position) < 1.0
+	
+	if close_enough:
+		var new_random_vector = Vector2(rng.randf_range(-max_jiggle_size, max_jiggle_size), rng.randf_range(-max_jiggle_size, max_jiggle_size))
+		target_position = Vector2(node_base_coord) + new_random_vector
+		
+		var pos_tween = get_tree().create_tween()
+		pos_tween.tween_property(self, "global_position", target_position, min_jiggle_time + randf_range(0, min_jiggle_time)).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+
 func activate_node() -> void:
 	puzzle_sprite.texture = ACTIVE_TEXTURE
 	var zoom_tween = get_tree().create_tween()
-	zoom_tween.tween_property(puzzle_sprite, "scale", Vector2(1.2, 1.2), 0.2)
+	zoom_tween.tween_property(puzzle_sprite, "scale", Vector2(1.2, 1.2), 0.1).set_ease(Tween.EASE_IN_OUT)
 
 
 func deactivate_node() -> void:
 	puzzle_sprite.texture = NORMAL_TEXTURE
 	var zoom_tween = get_tree().create_tween()
-	zoom_tween.tween_property(puzzle_sprite, "scale", Vector2(1.0, 1.0), 0.2)
+	zoom_tween.tween_property(puzzle_sprite, "scale", Vector2(1.0, 1.0), 0.1).set_ease(Tween.EASE_IN_OUT)
+
+
+func disable_node() -> void:
+	if node_enabled:
+		node_enabled = false
+		var zoom_tween = get_tree().create_tween()
+		zoom_tween.tween_property(puzzle_sprite, "scale", Vector2(0.8, 0.8), 0.1).set_ease(Tween.EASE_IN_OUT)
+		
+		var color_tween = get_tree().create_tween()
+		color_tween.tween_property(puzzle_sprite, "modulate", Color(0.6, 0.6, 0.6, 0.8), 0.1)
+
+
+func enable_node() -> void:
+	if not node_enabled:
+		node_enabled = true
+		activate_node()
+
+
+func _check_to_enabled() -> void:
+	if not node_enabled:
+		enable_node()
